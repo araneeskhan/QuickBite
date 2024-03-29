9285f9fbee12506bfb423cb50174b50037f6d0c5
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2024-03-29 23:59:59 +0500

    Committing changes

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {memo} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import TabNavigator from './src/navigators/TabNavigator';
@@ -7,26 +7,45 @@ import SignUp from './src/screens/SignUp';
 import DetailsScreen from './src/screens/DetailsScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
 import OnBoarding from './src/screens/OnBoarding';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
-const AuthFlow = () => (
-  <Stack.Navigator screenOptions={{headerShown: false}}>
+const AuthFlow = memo(() => (
+  <Stack.Navigator 
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#fff' },
+      animationEnabled: true,
+    }}
+  >
     <Stack.Screen name="OnBoarding" component={OnBoarding} />
     <Stack.Screen name="SignUp" component={SignUp} />
     <Stack.Screen name="SignIn" component={SignIn} />
   </Stack.Navigator>
-);
+));
+
+const MainStack = memo(() => (
+  <Stack.Navigator 
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#fff' },
+      animationEnabled: true,
+    }}
+  >
+    <Stack.Screen name="AuthFlow" component={AuthFlow} />
+    <Stack.Screen name="Home" component={TabNavigator} />
+    <Stack.Screen name="Details" component={DetailsScreen} />
+    <Stack.Screen name="Payment" component={PaymentScreen} />
+  </Stack.Navigator>
+));
 
 const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="AuthFlow" component={AuthFlow} />
-      <Stack.Screen name="Home" component={TabNavigator} />
-      <Stack.Screen name="Details" component={DetailsScreen} />
-      <Stack.Screen name="Payment" component={PaymentScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
+  <SafeAreaProvider>
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  </SafeAreaProvider>
 );
 
-export default App;
+export default memo(App);
