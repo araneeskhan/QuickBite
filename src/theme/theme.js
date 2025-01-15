aa44a2322622544953814a6726d7d9f98a10e835
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2025-01-15 23:59:59 +0500

    Committing changes

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,5 +1,29 @@
 import {DefaultTheme} from 'react-native-paper';
 
+export const FONTFAMILY = {
+  poppins_black: 'Poppins-Black',
+  poppins_bold: 'Poppins-Bold',
+  poppins_extrabold: 'Poppins-ExtraBold',
+  poppins_extralight: 'Poppins-ExtraLight',
+  poppins_light: 'Poppins-Light',
+  poppins_medium: 'Poppins-Medium',
+  poppins_regular: 'Poppins-Regular',
+  poppins_semibold: 'Poppins-SemiBold',
+  poppins_thin: 'Poppins-Thin',
+};
+
+export const COLORS = {
+  primaryDark: '#FF6B6B',
+  primaryLight: '#FF8787',
+  secondaryDark: '#4ECDC4',
+  secondaryLight: '#7BDFF2',
+  dark: '#2C3E50',
+  light: '#F8F9FA',
+  white: '#FFFFFF',
+  black: '#000000',
+  grey: '#A9A9A9',
+};
+
 export const theme = {
   ...DefaultTheme,
   colors: {
